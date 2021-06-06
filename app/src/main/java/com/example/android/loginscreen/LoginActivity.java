8f2c2bfc4c459af7559c79566a017ab40af3fb20
ayushwant <ayushwant912@gmail.com>
2021-06-07 00:45:39 +0530

    google sign in done

--- app/src/main/java/com/example/android/loginscreen/LoginActivity.java
+++ app/src/main/java/com/example/android/loginscreen/LoginActivity.java
@@ -1,19 +1,41 @@
 package com.example.android.loginscreen;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginActivity extends AppCompatActivity {
 
+    public static final int GOOGLE_SIGN_IN_CODE = 10005;
     TabLayout tabLayout;
     ViewPager viewPager;
     FloatingActionButton fb, google;
     float v=0;
+
+    GoogleSignInOptions gso ;
+    GoogleSignInClient signInClient;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +46,7 @@ public class LoginActivity extends AppCompatActivity {
         viewPager = findViewById(R.id.view_pager);
         fb = findViewById(R.id.fab_fb);
         google = findViewById(R.id.fab_google);
+        firebaseAuth = FirebaseAuth.getInstance();
 
         tabLayout.addTab(tabLayout.newTab().setText("Login"));
         tabLayout.addTab(tabLayout.newTab().setText("SignUp"));
@@ -41,11 +64,79 @@ public class LoginActivity extends AppCompatActivity {
         google.setAlpha(v);
         tabLayout.setAlpha(v);
 
-
         fb.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(400).start();
         google.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(400).start();
 
 
+        // <For google sign in>
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("992411915707-6th6g3ca5tij4brs0gq1js37m9o30b83.apps.googleusercontent.com")
+                .requestEmail().build();
+        signInClient = GoogleSignIn.getClient(this, gso);
+
+        GoogleSignInAccount acc = GoogleSignIn.getLastSignedInAccount(this);
+
+        if(acc!=null || firebaseAuth.getCurrentUser()!=null) // means already signed in
+        {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        }
+          // sign in him/her
+            google.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent sign = signInClient.getSignInIntent(); // get intent
+
+                    // get user account and connect
+                    startActivityForResult(sign, GOOGLE_SIGN_IN_CODE);
+
+                }
+            });
+
+
+    }
+
+
+    // to handle the data we get from signing in, we need this method
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==GOOGLE_SIGN_IN_CODE)
+        {
+            Task<GoogleSignInAccount> signInTask = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            // check if account connected
+            try {
+                GoogleSignInAccount signInAcc = signInTask.getResult(ApiException.class);
+
+                // this we need to do for data to get uploaded on firestore
+                AuthCredential authCredential = GoogleAuthProvider.getCredential(signInAcc.getIdToken(), null);
+
+                // register now
+                firebaseAuth.signInWithCredential(authCredential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Toast.makeText(getApplicationContext(), "Your acc is signed in", Toast.LENGTH_SHORT);
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(), "Try signing in again", Toast.LENGTH_SHORT);
+                    }
+                });
+
+
+
+//                Toast.makeText(this, "Your acc is signed in", Toast.LENGTH_SHORT);
+//                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+
+
+            } catch (ApiException e) {
+                e.printStackTrace();
+            }
+
+        }
 
     }
 }
